be73e844802b51d25d36bbe7c50006b3ad781848
Phanquan481 <phanquan481@gmail.com>
2022-10-23 16:34:17 +0700

    fix heroku

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,3 +1,3 @@
-export { default as Text } from "./text";
 export { default as CircleProgress } from "./circle-progress";
+export { default as Text } from "./text";
 export { default as MovieListItemTypeA } from "../page/home/component/movie-list-item-type-a";
