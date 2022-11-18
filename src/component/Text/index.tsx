09f50dbf4bd91b6fb64ca3111fa3a85996b22803
PVQuy <pvquy@cmcglobal.vn>
2022-11-18 13:45:55 +0700

    format code

--- src/component/Text/index.tsx
+++ src/component/Text/index.tsx
@@ -1,12 +1,8 @@
-import { TextStyled } from "./styled"
-import { TextProps } from "./type"
+import { TextStyled } from "./styled";
+import { TextProps } from "./type";
 
+const Text = ({ ...props }: TextProps) => {
+    return <TextStyled {...props} />;
+};
 
-
-const Text = ({...props}: TextProps) => {
-    return (
-            <TextStyled {...props}/>
-    )
-}
-
-export default Text
+export default Text;
--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,3 +1,3 @@
 export { default as CircleProgress } from "./circle-progress";
-export { default as Text } from "./text";
+export { default as Text } from "./Text";
 export { default as MovieListItemTypeA } from "../page/home/component/movie-list-item-type-a";
