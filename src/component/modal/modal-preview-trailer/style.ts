5b31b0b5418ce5d58e8585c9abaa31db660330a3
Phanquan481 <phanquan481@gmail.com>
2022-12-14 06:43:45 +0700

    fix modal preview

--- src/component/modal/modal-preview-trailer/style.ts
+++ src/component/modal/modal-preview-trailer/style.ts
@@ -1,21 +1,32 @@
 import styled from "styled-components";
 import { Modal } from "antd";
+import Constant from "util/Constants";
 
 export const ModalCustomStyle = styled(Modal)`
+  width: 95% !important;
+  top: 20vh;
+  .ant-modal-content {
+    .ant-modal-body {
+      height: 30vh;
+      padding: 0;
+
+      .modal-title-content {
+        background-color: black;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 0.875rem;
+      }
+    }
+  }
+
+  @media ${Constant.SCREEN.LARGE} {
     width: 70% !important;
     top: 0.5rem;
     .ant-modal-content {
-        .ant-modal-body {
-            height: 85vh;
-            padding: 0;
-
-            .modal-title-content {
-                background-color: black;
-                display: flex;
-                flex-direction: row;
-                justify-content: space-between;
-                padding: 0.875rem;
-            }
-        }
+      .ant-modal-body {
+        height: 85vh;
+      }
     }
+  }
 `;
