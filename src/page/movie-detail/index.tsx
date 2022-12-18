b1a6fe59f2cd2d4d2d953dc93d33acc675763947
Phanquan481 <phanquan481@gmail.com>
2022-12-18 17:28:53 +0700

    test cors

--- src/page/movie-detail/index.tsx
+++ src/page/movie-detail/index.tsx
@@ -1,44 +1,30 @@
-
 import { FastAverageColor } from "fast-average-color";
 import { useEffect, useRef } from "react";
 
 const MovieDetailPage = () => {
   const posterRef = useRef<any>();
-  //   useEffect(() => {
-  //     const fac = new FastAverageColor();
-  //     // fac
-  //     //   .getColorAsync(posterRef.current)
-  //     //   .then((color) => {
-  //     //     console.log("+++ color", color);
-  //     //   })
-  //     //   .catch((e) => {
-  //     //     console.log("+++ err", e);
-  //     //   });
-  //     const container = document.querySelector("#test");
+  useEffect(() => {
+    const src = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mLbFg0u2DPYuB5CILzwWk3kdI8b.jpg";
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',
+        // "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+        // "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+      },
+    };
 
-  //     // console.log("+++ color", fac.getColor(container!!.querySelector("img")));
-
-  //     const colorThief = new ColorThief();
-  //     const img = container!!.querySelector("img");
-  //     // img?.setAttribute("crossOrigin", "");
-  //     img!!.onload = () => {
-  //         console.log("+++ img onload", img);
-  //     }
-  //     img!!.src =
-  //       "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mLbFg0u2DPYuB5CILzwWk3kdI8b.jpg";
-  //     img?.setAttribute("crossOrigin", "");
-
-  //     fac
-  //       .getColorAsync(img)
-  //       .then((color) => {
-  //         console.log("+++ fast avg", color);
-  //       })
-  //       .catch((e) => {
-  //         console.log("+++ error", e);
-  //       });
-
-  //     // console.log('+++ colorThief.getPalette(img);', colorThief.getColor(img));
-  //   }, [posterRef.current]);
+    fetch(src, options)
+      .then((res) => {
+          console.log('+++ res', res);
+          
+          return res.blob()
+      })
+      .then((blob) => {
+        console.log('+++ fetch', URL.createObjectURL(blob));
+        
+      });
+  }, [posterRef.current]);
 
   const handleOnLoadImg = () => {
     const container = document.querySelector("#test");
@@ -53,14 +39,14 @@ const MovieDetailPage = () => {
 
   return (
     <div id="test">
-      <img
+      {/* <img
         onLoad={handleOnLoadImg}
         id="poster"
         ref={posterRef}
         src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mLbFg0u2DPYuB5CILzwWk3kdI8b.jpg"
         alt=""
         // crossOrigin="anonymous"
-      />
+      /> */}
     </div>
   );
 };
