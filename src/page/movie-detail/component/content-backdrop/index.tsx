a813180786f223573dc04e6d31e8aeed5b7a6d45
PVQuy <pvquy@cmcglobal.vn>
2022-12-23 09:52:03 +0700

    fix ui

--- src/page/movie-detail/component/content-backdrop/index.tsx
+++ src/page/movie-detail/component/content-backdrop/index.tsx
@@ -3,58 +3,66 @@ import { FastAverageColor } from "fast-average-color";
 import { MovieDetail } from "model/movie";
 import { useEffect, useState } from "react";
 import Constant from "util/Constants";
-import { MobileContentBackdrop } from "./styled";
+import { MobileContent, MobileContentBackdrop } from "./styled";
 
 interface IProps {
-  movieDetail?: MovieDetail;
+    movieDetail?: MovieDetail;
 }
 
 const ContentBackDrop = ({ movieDetail }: IProps) => {
-  console.log("+++ movieDetail", movieDetail?.backdrop_path);
-  const [test, settest] = useState('')
+    console.log("+++ movieDetail", movieDetail?.backdrop_path);
 
-  useEffect(() => {
-    // Get root element
-    const root: HTMLElement | null = document.querySelector(":root");
-    // Set default value css variable
-    root?.style.setProperty("--bg-content-backdrop", "white");
-    root?.style.setProperty("--txt-backdrop", "black");
-    if (movieDetail?.poster_path) {
-        settest(movieDetail.backdrop_path)
-      const fac = new FastAverageColor();
-      // Get average color from poster path
-      fac
-        .getColorAsync(
-          `https://image.tmdb.org/t/p/w220_and_h330_face${movieDetail.poster_path}`
-        )
-        .then((color) => {
-          // Set average value css variable
-          root?.style.setProperty("--bg-content-backdrop", String(color.hex));
-          root?.style.setProperty(
-            "--txt-backdrop",
-            `${color.isDark ? "white" : "black"}`
-          );
-        })
-        .catch((e) => {
-          // Do Something
-        });
-    }
-  }, [movieDetail]);
-  return (
-    <MobileContentBackdrop
-      backdropImage={`${Constant.IMAGE_PATH_W1000_h450}${movieDetail?.backdrop_path}`}
-    >
-      <img
-        className="backdrop-poster"
-        src={`${Constant.IMAGE_PATH_W220}${movieDetail?.poster_path}`}
-        alt="poster"
-      />
-      <div className="backdrop-gradient"/>
-      {/* <Text className="backdrop-text" size="tiny" color="white">
-        Content
-      </Text> */}
-    </MobileContentBackdrop>
-  );
+    useEffect(() => {
+        // Get root element
+        const root: HTMLElement | null = document.querySelector(":root");
+        // Set default value css variable
+        root?.style.setProperty("--bg-content-backdrop", "white");
+        root?.style.setProperty("--txt-backdrop", "black");
+        if (movieDetail?.poster_path) {
+            const fac = new FastAverageColor();
+            // Get average color from poster path
+            fac.getColorAsync(
+                `https://image.tmdb.org/t/p/w220_and_h330_face${movieDetail.poster_path}`
+            )
+                .then((color) => {
+                    // Set average value css variable
+                    root?.style.setProperty(
+                        "--bg-content-backdrop",
+                        String(color.hex)
+                    );
+                    root?.style.setProperty(
+                        "--txt-backdrop",
+                        `${color.isDark ? "white" : "black"}`
+                    );
+                })
+                .catch((e) => {
+                    // Do Something
+                });
+        }
+    }, [movieDetail]);
+    return (
+        <MobileContent>
+            <MobileContentBackdrop
+                backdropImage={`${Constant.IMAGE_PATH_W1000_h450}${movieDetail?.backdrop_path}`}
+            >
+                <div className="backdrop-gradient">
+                    <img
+                        className="backdrop-poster"
+                        src={`${Constant.IMAGE_PATH_W220}${movieDetail?.poster_path}`}
+                        alt="poster"
+                    />
+                </div>
+            </MobileContentBackdrop>
+
+            <Text
+                className="content-text movie-title"
+                size="medium"
+                weight="bold"
+            >
+                {movieDetail?.title}
+            </Text>
+        </MobileContent>
+    );
 };
 
 export default ContentBackDrop;
--- src/page/movie-detail/component/content-backdrop/styled.ts
+++ src/page/movie-detail/component/content-backdrop/styled.ts
@@ -2,39 +2,51 @@ import styled from "styled-components";
 import Constant from "util/Constants";
 
 interface ContentBackdropProps {
-  backdropImage?: string;
+    backdropImage?: string;
 }
 
+export const MobileContent = styled.div`
+    background-color: var(--bg-content-backdrop);
+    height: 500px;
+    .content-text {
+        color: var(--txt-backdrop);
+    }
+    .movie-title {
+      margin: 0.5rem 0;
+      font-size: 1.375rem;
+      font-weight: unset;
+      text-align: center;
+    }
+`;
+
 export const MobileContentBackdrop = styled.div<ContentBackdropProps>`
-  background-color: var(--bg-content-backdrop);
-  position: relative;
-  /* height: 300px; */
-  background-image: ${(props: ContentBackdropProps) =>
-    `url(${props.backdropImage})`};
-  /* background-image: url('https://image.tmdb.org/t/p/original/nfdVrxicFrwIbWV4856btQA1IBX.jpg'); */
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 10.5rem;
-  .backdrop-poster {
-    position: absolute;
-    top: 1.25rem;
-    left: 1.25rem;
-    width: calc(((100vw / 2.222222) - 40px) / 1.5);
-    min-width: calc(((100vw / 2.222222) - 40px) / 1.5);
-    height: calc((100vw / 2.222222) - 40px);
-    min-height: calc((100vw / 2.222222) - 40px);
-    border-radius: ${Constant.STYLED.BORDER_RADIUS_POSTER};
-  }
-  .backdrop-gradient {
-    height: 100%;
-    background-image: linear-gradient(
-      to right,
-      var(--bg-content-backdrop) 20%,
-      rgba(31.5, 31.5, 31.5, 0) 50%
-    );
-  }
-  .backdrop-text {
-    color: var(--txt-backdrop);
-  }
+    background-color: var(--bg-content-backdrop);
+    position: relative;
+    /* height: 300px; */
+    background-image: ${(props: ContentBackdropProps) =>
+        `url(${props.backdropImage})`};
+    /* background-image: url('https://image.tmdb.org/t/p/original/nfdVrxicFrwIbWV4856btQA1IBX.jpg'); */
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 10.5rem;
+    .backdrop-gradient {
+        height: 100%;
+        background-image: linear-gradient(
+            to right,
+            var(--bg-content-backdrop) 10%,
+            rgba(31.5, 31.5, 31.5, 0) 50%
+        );
+
+        .backdrop-poster {
+            position: absolute;
+            top: 1.25rem;
+            left: 1.25rem;
+            width: calc(((100vw / 2.222222) - 40px) / 1.5);
+            min-width: calc(((100vw / 2.222222) - 40px) / 1.5);
+            height: calc((100vw / 2.222222) - 40px);
+            min-height: calc((100vw / 2.222222) - 40px);
+            border-radius: ${Constant.STYLED.BORDER_RADIUS_POSTER};
+        }
+    }
 `;
